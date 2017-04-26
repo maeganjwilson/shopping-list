2bac48a78f7c96e1758f162ea10c6bdc0fef110d
maeganjwilson <maegan@maeganwilson.com>
2017-04-26 11:41:30 -0500

    - Changed source and JQuery - Can add item!

--- app.js
+++ app.js
@@ -14,40 +14,30 @@ var state = {
 
 // FUNCTIONS
 var addItem = function(state, item) {
-  var temp = '<li>' +
-    '<span class="shopping-item">' + item + '</span>' +
-    '<div class="shopping-item-controls">' +
-    '<button class="shopping-item-toggle">' +
-    '<span class="button-label">check</span>' +
-    '</button>' +
-    '<button class="shopping-item-delete">' +
-    '<span class="button-label">delete</span>' +
-    '</button>' +
-    '</div>' +
-    '</li>';
-
-  state.push(temp);
+  state.items.push(item);
 };
 
-var removeItem = function(state, item) {
-  //NEED TO LOOK INSIDE STATE FIND ITEM AND REMOVE
-  //Will need position
-  item.splice(i, 1);
-}
-
-var checkItem = function(state, item) {
-  /*
-   *
-   * NEED TO LOOK INSIDE STATE FIND STATE AND TOGGLE THE
-   * shopping-item__checked class
-   * NEED ITEM
-   *
-   */
-}
-
 // RENDER
-
+var renderList = function(state, element) {
+    var itemsHTML = state.items.map(function(item) {
+      return '<li>' +
+        '<span class="shopping-item">' + item + '</span>' +
+        '<div class="shopping-item-controls">' +
+        '<button class="shopping-item-toggle">' +
+        '<span class="button-label">check</span>' +
+        '</button>' +
+        '<button class="shopping-item-delete">' +
+        '<span class="button-label">delete</span>' +
+        '</button>' +
+        '</div>' +
+        '</li>';
+    });
+    element.html(itemsHTML);
+};
 // Event listeners
 // Add
-// Remove
-// Check
+$('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    addItem(state, $('#shopping-list-entry').val());
+    renderList(state, $('.shopping-list'));
+});
