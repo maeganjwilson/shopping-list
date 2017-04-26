204cebb82332b5d41aad3f0776a41e267994cd45
maeganjwilson <maegan@maeganwilson.com>
2017-04-25 21:16:09 -0500

    Added spaces

--- app.js
+++ app.js
@@ -9,28 +9,28 @@
 
 // STATE
 var state = {
-  items : []
+    items: []
 };
 
 // FUNCTIONS
 var addItem = function(state, item) {
-  state.items.push(item);
+    state.items.push(item);
 };
 
-var removeItem = function(state, item){
-  //NEED TO LOOK INSIDE STATE FIND ITEM AND REMOVE
-  //Will need position
-  item.splice(i,1);
+var removeItem = function(state, item) {
+    //NEED TO LOOK INSIDE STATE FIND ITEM AND REMOVE
+    //Will need position
+    item.splice(i, 1);
 }
 
-var checkItem = function(state, item){
-  /*
-  *
-  * NEED TO LOOK INSIDE STATE FIND STATE AND TOGGLE THE
-  * shopping-item__checked class
-  * NEED ITEM
-  *
-  */
+var checkItem = function(state, item) {
+    /*
+     *
+     * NEED TO LOOK INSIDE STATE FIND STATE AND TOGGLE THE
+     * shopping-item__checked class
+     * NEED ITEM
+     *
+     */
 }
 
 // RENDER
