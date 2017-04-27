055b1205b56b3710e24f705a32590837f752319a
maeganjwilson <maegan@maeganwilson.com>
2017-04-26 23:03:56 -0500

    Went back and made outlines of what I will need

--- app.js
+++ app.js
@@ -6,38 +6,60 @@
  *  4. Remove item
  *
  *******/
+//template
+var listItemTemplate = (
+  '<li>' +
+    '<span class="shopping-item"></span>' +
+      '<div class="shopping-item-controls">' +
+      '<button class="shopping-item-toggle">' +
+        '<span class="button-label">check</span>' +
+      '</button>' +
+      '<button class="shopping-item-delete">' +
+        '<span class="button-label">delete</span>' +
+      '</button>' +
+    '</div>' +
+  '</li>'
+);
 
 // STATE
 var state = {
   items: []
 };
 
-// FUNCTIONS
+//FUNCTION TO ADD
 var addItem = function(state, item) {
-  state.items.push(item);
+  state.items.push({
+    itemName: item,
+    checked: false //added to keep track of an item that is checked/not checked
+  });
+};
+
+//TO CHECK
+var checkItem(state, item){
+  //Need to change checked to true
+};
+//TO REMOVE
+var deleteItem(state, itemIndex){
+  //Remove item from array
+  // there is a splice method I can use
 };
 
 // RENDER
 var renderList = function(state, element) {
-    var itemsHTML = state.items.map(function(item) {
-      return '<li>' +
-        '<span class="shopping-item">' + item + '</span>' +
-        '<div class="shopping-item-controls">' +
-        '<button class="shopping-item-toggle">' +
-        '<span class="button-label">check</span>' +
-        '</button>' +
-        '<button class="shopping-item-delete">' +
-        '<span class="button-label">delete</span>' +
-        '</button>' +
-        '</div>' +
-        '</li>';
-    });
-    element.html(itemsHTML);
+  //Goes through each item and outputs the array
 };
+
+//----------------
 // Event listeners
+//
 // Add
 $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
     addItem(state, $('#shopping-list-entry').val());
     renderList(state, $('.shopping-list'));
+});
+
+//check
+$('.shopping-item-toggle').click(function(event) {
+  checkItem(state, this.parent('li').val());
 });
