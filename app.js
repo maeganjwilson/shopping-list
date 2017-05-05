79d79f3c63f4cc339c400a15b9d5631240e10d24
maeganjwilson <maegan@maeganwilson.com>
2017-05-04 19:00:43 -0500

    Adding works. Clicking on check does not work on added items

--- app.js
+++ app.js
@@ -8,31 +8,50 @@
  *******/
 
 // STATE
-var state = {
-  items: []
-};
+var state = [];
 
 // FUNCTIONS
 var addItem = function(state, item) {
-  state.items.push(item);
+  state.push(
+    {
+      itemName: item,
+      checked: false //not checked
+    }
+  );
 };
 
+
+var item = {itemName:""};
+
+var template = '<li>' +
+  '<span class="shopping-item">' + item.itemName + '</span>' +
+  '<div class="shopping-item-controls">' +
+  '<button class="shopping-item-toggle">' +
+  '<span class="button-label">check</span>' +
+  '</button>' +
+  '<button class="shopping-item-delete">' +
+  '<span class="button-label">delete</span>' +
+  '</button>' +
+  '</div>' +
+  '</li>';
+template = $(template);
 // RENDER
 var renderList = function(state, element) {
-    var itemsHTML = state.items.map(function(item) {
-      return '<li>' +
-        '<span class="shopping-item">' + item + '</span>' +
-        '<div class="shopping-item-controls">' +
-        '<button class="shopping-item-toggle">' +
-        '<span class="button-label">check</span>' +
-        '</button>' +
-        '<button class="shopping-item-delete">' +
-        '<span class="button-label">delete</span>' +
-        '</button>' +
-        '</div>' +
-        '</li>';
-    });
-    element.html(itemsHTML);
+  var currentItem = template.clone();
+  currentItem.find('.shopping-item').html($('#shopping-list-entry').val());
+  currentItem.find('.shopping-item-toggle').on('.click', function() {
+
+      if(currentItem.checked){
+        currentItem.checked = true;
+        currentItem.find('.shopping-item').addClass('shopping-item__checked');
+      }
+      else {
+        currentItem.checked = false;
+        currentItem.find('.shopping-item').removeClass('shopping-item__checked');
+      }
+      return currentItem;
+  });
+    element.append(currentItem); //creates via template
 };
 // Event listeners
 // Add
@@ -40,4 +59,8 @@ $('#js-shopping-list-form').submit(function(event) {
     event.preventDefault();
     addItem(state, $('#shopping-list-entry').val());
     renderList(state, $('.shopping-list'));
+});
+//check
+$('.shopping-item-toggle').click(function(){
+  alert("click works");
 });
